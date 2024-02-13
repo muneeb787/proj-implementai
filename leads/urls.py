2cd33c573e47bef55bfe4271d5e02836c11b0735
Muneeb <muneebahmad1268@gmail.com>
2024-02-13 15:49:14 +0500

    Revised APi (Resolve leads api issue)

--- leads/urls.py
+++ leads/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create_lead, name='create_lead'),
-    path('<int:lead_id>/', views.get_lead_by_id, name='get_lead'),
-    path('getAll', views.get_leads_list, name='get_lead'),
+    path('create', views.create_lead, name='create_lead'),
+    path('<int:lead_id>', views.get_lead_by_id, name='get_lead'),
+    path('list/', views.get_leads_list, name='get_all_lead'),
     path('initiate-call/<int:lead_id>', views.initiate_phone_call, name='vapi_call_initiate'),
     path('callback', views.callback, name='vapi_callback'),
 ]
