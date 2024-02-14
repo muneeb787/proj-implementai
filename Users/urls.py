67fdda05486d7fd4e7f114418e498bd9eaa28687
Muneeb <muneebahmad1268@gmail.com>
2024-02-14 15:46:36 +0500

    Routes Fix

--- Users/urls.py
+++ Users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import get_All, get_by_Id, create_user,get_csrf_token,login
 
 urlpatterns = [
-    path('list/', get_All, name='user-list'),
-    path('<int:user_id>/', get_by_Id, name='user-detail'),
-    path('create/', create_user, name='create-user'),
-    path('get-csrf-token/', get_csrf_token, name='get_csrf_token'),
-    path('login/', login, name='user-login'),
+    path('list', get_All, name='user-list'),
+    path('<int:user_id>', get_by_Id, name='user-detail'),
+    path('create', create_user, name='create-user'),
+    path('get-csrf-token', get_csrf_token, name='get_csrf_token'),
+    path('login', login, name='user-login'),
 ]
