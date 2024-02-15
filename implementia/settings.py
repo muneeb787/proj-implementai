8559ac79c40d40e9324d77a2eede1ea2d1c976b6
Muneeb <muneebahmad1268@gmail.com>
2024-02-15 11:21:27 +0500

    Pagination On Get All leads

--- implementia/settings.py
+++ implementia/settings.py
@@ -89,6 +89,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 AUTHENTICATION_BACKENDS = [
